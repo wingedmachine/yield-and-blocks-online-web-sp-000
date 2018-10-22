3b695c07e45ed78e9b41f8b300239e155a9f91ea
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 22:19:20 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(array)
+  if !block_given?
+    puts "Hey! No block was given!"
+  else
+    i = 0
+    while i < array.size
+      yield array[i]
+      i += 1
+    end
+  end
+  array
 end
 
 # call your method here!
-
+hello_t(%w[Tom Tim Jim]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
